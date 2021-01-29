098e15f106462e0a189611bc23b70a0bb20931e0
Navpreet Singh <navpreet.mattu@outlook.com>
2021-01-29 23:08:24 +0530

    HashTable changes

--- src/main/java/com/navpreet/demo/Main.java
+++ src/main/java/com/navpreet/demo/Main.java
@@ -1,15 +1,15 @@
 package com.navpreet.demo;
 
-import java.util.Arrays;
-
-import com.navpreet.demo.array.MergeSortedArray;
+import com.navpreet.demo.hashtable.HashTable;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] a1 = { 2, 5, 8, 12, 45, 77, 89, 98, 101, 102};
-        int[] a2 = { 3, 5, 7, 23, 34, 78, 99, 100 };
-        int[] a3 = MergeSortedArray.mergeSortedArrays(a1, a2);
-        System.out.println(Arrays.toString(a3));
+        var table = new HashTable<String, Integer>();
+        table.put("Navpreet", 25);
+        table.put("Singh", 34);
+
+        System.out.println(table.get("Navpreet"));
+        System.out.println(table.get("Singh"));
     }
 }
--- src/main/java/com/navpreet/demo/hashtable/HashTable.java
+++ src/main/java/com/navpreet/demo/hashtable/HashTable.java
@@ -2,7 +2,32 @@ package com.navpreet.demo.hashtable;
 
 public class HashTable<K, V> {
 
+    private static final int DEFAULT_BUCKET_SIZE = 16;
+
     private Entry<K, V>[] entry;
+
+    @SuppressWarnings("unchecked")
+    public HashTable() {
+        this.entry = new Entry[DEFAULT_BUCKET_SIZE];
+    }
+
+    public void put(K key, V value) {
+        int bucket = hashCode(key);
+        this.entry[bucket] = new Entry<>(key, value);
+    }
+
+    public V get(K key) {
+        var bucket = hashCode(key);
+        var en = this.entry[bucket];
+        if (en != null && en.getKey().equals(key)) {
+            return en.getValue();
+        }
+        return null;
+    }
+
+    private int hashCode(K key) {
+        return Math.abs(1 * key.hashCode()) % DEFAULT_BUCKET_SIZE;
+    }
 
     static class Entry<K, V> {
         private K key;
